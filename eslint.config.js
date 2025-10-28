435fb1409bb78f44d9789e7b270cfd6365d49081
kimeodml <kimeodml@gmail.com>
2025-10-28 20:25:06 +0900

    [Feat] Vue 세팅

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginVue from 'eslint-plugin-vue';
+import { defineConfig } from 'eslint/config';
+import { importX } from 'eslint-plugin-import-x';
+
+export default defineConfig([
+  pluginVue.configs['flat/essential'],
+  {
+    ignores: ['node_modules/**', 'dist/**'],
+    files: ['**/*.{js,mjs,cjs,vue}'],
+    plugins: { js },
+    extends: ['js/recommended', importX.flatConfigs.recommended],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'vue/no-undef-components': [
+        'error',
+        {
+          ignorePatterns: ['custom(\\-\\w+)+'],
+        },
+      ],
+    },
+  },
+]);
---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  안녕
+
+  <button>버튼임</button>
+  <a href="www.naver.com">안녕</a>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
