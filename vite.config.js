eec27931353edefa436b275e525e67c1f4f5e8a7
kimeodml <kimeodml@gmail.com>
2025-10-31 09:08:49 +0900

    feat: 환변경수별 스크립트 추가

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { fileURLToPath, URL } from 'url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   alias: {
-    '@': fileURLToPath(new URL('./src', import.meta.url))
-  }
-})
+    '@': fileURLToPath(new URL('./src', import.meta.url)),
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // 실제 백엔드 서버
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
