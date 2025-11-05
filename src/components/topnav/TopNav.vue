22a896be805904d776b974597eb35bb6cc670569
kimeodml <kimeodml@gmail.com>
2025-11-05 15:14:54 +0900

    refactor: 불필요한 코드 제거

--- src/components/topnav/TopNav.vue
+++ src/components/topnav/TopNav.vue
@@ -21,27 +21,17 @@
       <Sheet>
         <SheetTrigger>
           <div class="relative rounded-full p-2 hover:bg-muted transition">
-            <Bell class="h-5 w-5 text-foreground" />
+            <Bell class="h-5 w-5 text-foreground cursor-pointer" />
             <span class="absolute top-1.5 right-1.5 h-2 w-2 rounded-full bg-red-500"></span>
           </div>
         </SheetTrigger>
         <SheetContent>
           <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your account and remove
-              your data from our servers.
-            </SheetDescription>
+            <SheetTitle>알림 목록</SheetTitle>
+            <SheetDescription> 알림 기록 모음 </SheetDescription>
           </SheetHeader>
         </SheetContent>
       </Sheet>
-      <button
-        class="relative rounded-full p-2 hover:bg-muted transition"
-        aria-label="notifications"
-      >
-        <Bell class="h-5 w-5 text-foreground" />
-        <span class="absolute top-1.5 right-1.5 h-2 w-2 rounded-full bg-red-500"></span>
-      </button>
 
       <NavigationMenu>
         <NavigationMenuList>
