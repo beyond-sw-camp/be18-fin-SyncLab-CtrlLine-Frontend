f047f19a47178c6bb639decfd189321cdf0c038b
kimeodml <kimeodml@gmail.com>
2025-11-19 12:11:36 +0900

    [Feat] 탭으로 페이지 구분

---
+++ src/pages/dashboard/FirstFactory.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>첫 번째 공장</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/pages/dashboard/Index.vue
+++ src/pages/dashboard/Index.vue
@@ -1,7 +1,74 @@
 <template>
-  <div>대쉬보드</div>
+  <Tabs v-model="currentTab" default-value="scheduler">
+    <TabsList>
+      <TabsTrigger value="scheduler">전체 일정</TabsTrigger>
+      <TabsTrigger v-for="factory in data" :key="factory.factoryCode" :value="factory.factoryCode">
+        {{ factory.factoryName }}
+      </TabsTrigger>
+    </TabsList>
+
+    <TabsContent value="scheduler"> 전체 일정 콘텐츠 </TabsContent>
+
+    <TabsContent
+      v-for="factory in data"
+      :key="factory.factoryCode + '-content'"
+      :value="factory.factoryCode"
+    >
+      <component :is="FACTORY_COMPONENT_MAP[factory.factoryCode]" />
+    </TabsContent>
+  </Tabs>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
-<style lang="scss" scoped></style>
+// import useGetFactoryList from '@/apis/query-hooks/factory/useGetFactoryList';
+import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
+import FirstFactory from '@/pages/dashboard/FirstFactory.vue';
+import SecondFactory from '@/pages/dashboard/SecondFactory.vue';
+import ThirdFactory from '@/pages/dashboard/ThirdFactory.vue';
+
+const route = useRoute();
+const router = useRouter();
+
+// const { data: factoryList } = useGetFactoryList();
+
+const currentTab = ref(route.query.tab ?? 'scheduler');
+
+const FACTORY_COMPONENT_MAP = {
+  F0001: FirstFactory,
+  F0002: SecondFactory,
+  F0003: ThirdFactory,
+};
+
+const data = [
+  {
+    factoryCode: 'F0001',
+    factoryName: '제 1공장',
+  },
+  {
+    factoryCode: 'F0002',
+    factoryName: '제 2공장',
+  },
+  {
+    factoryCode: 'F0003',
+    factoryName: '제 3공장',
+  },
+];
+
+// 탭이 바뀌면 URL 업데이트
+watch(currentTab, value => {
+  router.replace({
+    query: { tab: value },
+  });
+});
+
+// URL이 바뀌면 탭도 반영 (뒤로가기 대응)
+watch(
+  () => route.query.tab,
+  value => {
+    if (value) currentTab.value = value;
+  },
+);
+</script>
