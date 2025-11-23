50ba8b48f6ad0ac08c2f8b1492197e002a12911f
kimeodml <kimeodml@gmail.com>
2025-11-23 10:33:16 +0900

    [Feat] 생산 계획 목록 조회 api

---
+++ src/apis/query-functions/productionPlan.js
@@ -0,0 +1,20 @@
+import apiClient from '@/apis/query-functions/apiClient';
+
+export async function getProductionPlanList(params) {
+  const query = new URLSearchParams();
+
+  for (const [key, value] of Object.entries(params)) {
+    if (key === 'sort' && Array.isArray(value)) {
+      value.forEach(sortItem => {
+        query.append('sort', sortItem);
+      });
+    } else if (value !== undefined && value !== null && value !== '') {
+      query.append(key, value);
+    }
+  }
+
+  console.log('query', params);
+
+  const { data } = await apiClient.get(`/production-plans?${query.toString()}`);
+  return data.data;
+}
