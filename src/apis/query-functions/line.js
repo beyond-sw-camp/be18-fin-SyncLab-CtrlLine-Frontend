c923021dad6a3b331599bc6c783903fb225d9ece
inhwa1003 <iinhwa016@gmail.com>
2025-11-28 15:42:48 +0900

    [feat] query-functions 추가

--- src/apis/query-functions/line.js
+++ src/apis/query-functions/line.js
@@ -1,10 +1,23 @@
 import apiClient from '@/apis/query-functions/apiClient';
 import { buildQueryObject } from '@/utils/buildQueryObject';
 
+// 라인 목록 조회
 export async function getLineList(params) {
   const queryObj = buildQueryObject(params);
   const search = new URLSearchParams(queryObj);
 
   const { data } = await apiClient.get(`/lines?${search.toString()}`);
   return data.data;
+}
+
+// 라인 상세조회
+export async function getLine(lineCode) {
+  const { data } = await apiClient.get(`/lines/${lineCode}`);
+  return data.data;
+}
+
+// 라인 업데이트
+export async function updateLine(lineCode, params) {
+  const { data } = await apiClient.patch(`/lines/${lineCode}`, params);
+  return data;
 }
