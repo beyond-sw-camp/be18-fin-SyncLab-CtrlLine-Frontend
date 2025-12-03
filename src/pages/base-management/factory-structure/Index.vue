e48f2444992b77b8113858590dfe2e5b143d4a47
dobbyrr <zxr1033@gmail.com>
2025-12-03 16:45:16 +0900

    [Feat] 공장 계층구조도 공장별 분류

--- src/pages/base-management/factory-structure/Index.vue
+++ src/pages/base-management/factory-structure/Index.vue
@@ -1,7 +1,53 @@
 <template>
-  <div>공장 계층 구조도</div>
+  <section class="space-y-6">
+    <div class="flex flex-col gap-4">
+      <Tabs v-if="factories.length" v-model="selectedFactoryCode" class="w-fit">
+        <TabsList class="justify-start">
+          <TabsTrigger
+            v-for="factory in factories"
+            :key="factory.factoryCode"
+            :value="factory.factoryCode"
+            class="min-w-32"
+          >
+            {{ factory.factoryName }}
+          </TabsTrigger>
+        </TabsList>
+      </Tabs>
+
+      <div v-else class="text-sm text-gray-500">공장 목록을 불러오는 중입니다...</div>
+
+      <div class="rounded-xl border border-gray-200 bg-white p-6">
+        <h2 class="text-xl font-semibold text-gray-900">
+          {{ selectedFactory?.factoryName ?? '공장 계층 구조도' }}
+        </h2>
+        <p class="mt-2 text-sm text-gray-500">공장 계층 구조도가 이 영역에 표시됩니다.</p>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed, ref, watch } from 'vue';
 
-<style lang="scss" scoped></style>
+import useGetFactoryList from '@/apis/query-hooks/factory/useGetFactoryList';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
+const { data: factoryList } = useGetFactoryList();
+
+const factories = computed(() => factoryList.value?.content ?? []);
+const selectedFactoryCode = ref('');
+
+watch(
+  factories,
+  list => {
+    if (!selectedFactoryCode.value && list.length) {
+      selectedFactoryCode.value = list[0].factoryCode;
+    }
+  },
+  { immediate: true },
+);
+
+const selectedFactory = computed(() =>
+  factories.value.find(factory => factory.factoryCode === selectedFactoryCode.value),
+);
+</script>
