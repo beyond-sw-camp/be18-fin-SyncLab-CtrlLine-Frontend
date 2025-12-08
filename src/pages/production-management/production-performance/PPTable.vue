d300cf04e5efcd3b1e83a03ba840a7dafa05ae6b
inhwa1003 <iinhwa016@gmail.com>
2025-12-08 15:10:24 +0900

    [feat] 생산 실적 상세조회

---
+++ src/pages/production-management/production-performance/PPTable.vue
@@ -0,0 +1,66 @@
+<template>
+  <Table class="w-full table-fixed">
+    <TableHeader class="border-b-2 border-primary">
+      <TableRow>
+        <TableHead class="text-center">품목코드</TableHead>
+        <TableHead class="text-center">품목명</TableHead>
+        <TableHead class="text-center">규격</TableHead>
+        <TableHead class="text-center">단위</TableHead>
+        <TableHead class="text-center">총 수량</TableHead>
+        <TableHead class="text-center">생산 실적 수량</TableHead>
+        <TableHead class="text-center">불량 수량</TableHead>
+        <TableHead class="text-center">불량률</TableHead>
+      </TableRow>
+    </TableHeader>
+
+    <TableBody v-if="PPDetail">
+      <TableRow class="text-center border-b border-dotted border-gray-300">
+        <TableCell class="py-3 whitespace-nowrap overflow-hidden text-ellipsis">
+          {{ PPDetail.itemCode }}
+        </TableCell>
+        <TableCell class="py-3 whitespace-nowrap overflow-hidden text-ellipsis">
+          {{ PPDetail.itemName }}
+        </TableCell>
+        <TableCell class="py-3 whitespace-nowrap overflow-hidden text-ellipsis">
+          {{ PPDetail.itemSpecification }}
+        </TableCell>
+        <TableCell class="py-3 whitespace-nowrap overflow-hidden text-ellipsis">
+          {{ PPDetail.itemUnit }}
+        </TableCell>
+
+        <TableCell class="py-3 text-end">
+          {{ PPDetail.totalQty }}
+        </TableCell>
+        <TableCell class="py-3 text-end">
+          {{ PPDetail.performanceQty }}
+        </TableCell>
+        <TableCell class="py-3 text-end">
+          {{ PPDetail.defectiveQty }}
+        </TableCell>
+        <TableCell class="py-3 text-end">
+          {{ PPDetail.defectiveRate }}
+        </TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
+</template>
+
+<script setup>
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+
+// defineProps 이름 itemDetail -> PPDetail로 변경
+defineProps({
+  PPDetail: {
+    type: Object,
+    required: false,
+    default: () => ({}),
+  },
+});
+</script>
--- src/pages/production-management/production-performance/ProductionPerformanceDetail.vue
+++ src/pages/production-management/production-performance/ProductionPerformanceDetail.vue
@@ -1,135 +1,101 @@
 <template>
   <div class="flex justify-between items-center mb-6">
     <h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">생산실적 상세 조회</h3>
+  </div>
 
-    <div
-      v-if="detail"
-      class="flex items-center text-xs font-medium bg-gray-100 px-2 py-1 rounded-xl"
+  <div class="flex flex-col gap-8 md:flex-row">
+    <form
+      v-if="productionPerformanceDetail?.id"
+      id="productionPerformanceForm"
+      @submit="onSubmit"
+      class="flex-1 flex flex-col gap-10"
     >
-      {{ detail.documentNo }}
-    </div>
-  </div>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-2">
+        <div class="order-1 md:order-0">
+          <FormField name="factoryName" v-slot="{ componentField }">
+            <FormItem>
+              <FormLabel>공장명</FormLabel>
+              <FormControl>
+                <Input type="text" v-bind="componentField" readonly class="text-sm" />
+              </FormControl>
+            </FormItem>
+          </FormField>
+        </div>
 
-  <div v-if="isLoading" class="text-center py-20 text-gray-500">
-    <p>데이터를 불러오는 중입니다...</p>
-  </div>
-  <div v-else-if="isError" class="text-center py-20 text-red-600">
-    <p>데이터 로딩 중 오류가 발생했습니다. (ID: {{ route.params.id }})</p>
-  </div>
+        <div class="order-4 md:order-0">
+          <FormField name="dueDate" v-slot="{ componentField }">
+            <FormItem>
+              <FormLabel>납기일</FormLabel>
+              <FormControl>
+                <Input type="date" v-bind="componentField" readonly class="text-sm" />
+              </FormControl>
+            </FormItem>
+          </FormField>
+        </div>
 
-  <Form
-    as="form"
-    v-else-if="detail"
-    :initial-values="{
-      // 상단 기본 정보
-      factoryName: `${detail.factoryName} (${detail.factoryCode})`,
-      lineName: `${detail.lineName} (${detail.lineCode})`,
-      dueDate: detail.dueDate,
-      salesManagerName: `${detail.salesManagerName} (${detail.salesManagerNo})`,
-      productionManagerName: `${detail.productionManagerName} (${detail.productionManagerNo})`,
-      startTime: detail.startTime,
-      endTime: detail.endTime,
-      lotNo: detail.lotNo,
-      remark: detail.remark,
-
-      // 하단 품목 및 생산 정보
-      itemCode: detail.itemCode,
-      itemName: detail.itemName,
-      itemSpecification: detail.itemSpecification,
-      itemUnit: detail.itemUnit,
-      totalQty: detail.totalQty,
-      performanceQty: detail.performanceQty,
-      defectiveQty: detail.defectiveQty,
-      defectiveRate: detail.defectRate,
-    }"
-  >
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-2">
-      <div class="order-1 md:order-0">
-        <FormField name="factoryName" v-slot="{ componentField }">
+        <FormField name="productionManagerName" v-slot="{ componentField }">
           <FormItem>
-            <FormLabel>공장명</FormLabel>
+            <FormLabel>생산담당자</FormLabel>
             <FormControl>
               <Input type="text" v-bind="componentField" readonly class="text-sm" />
             </FormControl>
           </FormItem>
         </FormField>
-      </div>
 
-      <div class="order-4 md:order-0">
-        <FormField name="dueDate" v-slot="{ componentField }">
+        <FormField name="itemName" v-slot="{ componentField }">
           <FormItem>
-            <FormLabel>납기일</FormLabel>
+            <FormLabel>품목명</FormLabel>
             <FormControl>
-              <Input type="date" v-bind="componentField" readonly class="text-sm" />
+              <Input type="text" v-bind="componentField" readonly class="text-sm" />
             </FormControl>
           </FormItem>
         </FormField>
-      </div>
 
-      <FormField name="productionManagerName" v-slot="{ componentField }">
-        <FormItem>
-          <FormLabel>생산담당자</FormLabel>
-          <FormControl>
-            <Input type="text" v-bind="componentField" readonly class="text-sm" />
-          </FormControl>
-        </FormItem>
-      </FormField>
+        <FormField name="startTime" v-slot="{ componentField }">
+          <FormItem>
+            <FormLabel>생산 시작시각</FormLabel>
+            <FormControl>
+              <Input type="datetime-local" v-bind="componentField" readonly class="text-sm" />
+            </FormControl>
+          </FormItem>
+        </FormField>
 
-      <FormField name="itemName" v-slot="{ componentField }">
-        <FormItem>
-          <FormLabel>품목명</FormLabel>
-          <FormControl>
-            <Input type="text" v-bind="componentField" readonly class="text-sm" />
-          </FormControl>
-        </FormItem>
-      </FormField>
+        <FormField name="salesManagerName" v-slot="{ componentField }">
+          <FormItem>
+            <FormLabel>영업담당자</FormLabel>
+            <FormControl>
+              <Input type="text" v-bind="componentField" readonly class="text-sm" />
+            </FormControl>
+          </FormItem>
+        </FormField>
 
-      <FormField name="startTime" v-slot="{ componentField }">
-        <FormItem>
-          <FormLabel>생산 시작시각</FormLabel>
-          <FormControl>
-            <Input type="datetime-local" v-bind="componentField" readonly class="text-sm" />
-          </FormControl>
-        </FormItem>
-      </FormField>
+        <FormField name="lineName" v-slot="{ componentField }">
+          <FormItem>
+            <FormLabel>라인명</FormLabel>
+            <FormControl>
+              <Input type="text" v-bind="componentField" readonly class="text-sm" />
+            </FormControl>
+          </FormItem>
+        </FormField>
 
-      <FormField name="salesManagerName" v-slot="{ componentField }">
-        <FormItem>
-          <FormLabel>영업담당자</FormLabel>
-          <FormControl>
-            <Input type="text" v-bind="componentField" readonly class="text-sm" />
-          </FormControl>
-        </FormItem>
-      </FormField>
+        <FormField name="endTime" v-slot="{ componentField }">
+          <FormItem>
+            <FormLabel>생산 종료시각</FormLabel>
+            <FormControl>
+              <Input type="datetime-local" v-bind="componentField" readonly class="text-sm" />
+            </FormControl>
+          </FormItem>
+        </FormField>
 
-      <FormField name="lineName" v-slot="{ componentField }">
-        <FormItem>
-          <FormLabel>라인명</FormLabel>
-          <FormControl>
-            <Input type="text" v-bind="componentField" readonly class="text-sm" />
-          </FormControl>
-        </FormItem>
-      </FormField>
+        <FormField name="lotNo" v-slot="{ componentField }">
+          <FormItem>
+            <FormLabel>LOT 번호</FormLabel>
+            <FormControl>
+              <Input type="text" v-bind="componentField" readonly class="text-sm" />
+            </FormControl>
+          </FormItem>
+        </FormField>
 
-      <FormField name="endTime" v-slot="{ componentField }">
-        <FormItem>
-          <FormLabel>생산 종료시각</FormLabel>
-          <FormControl>
-            <Input type="datetime-local" v-bind="componentField" readonly class="text-sm" />
-          </FormControl>
-        </FormItem>
-      </FormField>
-
-      <FormField name="lotNo" v-slot="{ componentField }">
-        <FormItem>
-          <FormLabel>LOT 번호</FormLabel>
-          <FormControl>
-            <Input type="text" v-bind="componentField" readonly class="text-sm" />
-          </FormControl>
-        </FormItem>
-      </FormField>
-
-      <div class="order-10 md:order-0 col-span-1 md:col-span-3">
         <FormField name="remark" v-slot="{ componentField }">
           <FormItem class="md:col-span-3">
             <FormLabel>비고</FormLabel>
@@ -139,29 +105,74 @@
           </FormItem>
         </FormField>
       </div>
-    </div>
-  </Form>
+
+      <div class="mt-6 border-t pt-4">
+        <h4 class="scroll-m-20 text-xl font-semibold mb-4">품목 및 생산, 불량 정보</h4>
+        <PPTable :PPDetail="PPDetail" />
+      </div>
+    </form>
+  </div>
 </template>
 
 <script setup>
-import { Form } from 'vee-validate';
+import { useForm } from 'vee-validate';
+import { ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 
 import useGetProductionPerformance from '@/apis/query-hooks/production-performance/useGetProductionPerformance';
-import useUpdateProductionPerformance from '@/apis/query-hooks/production-performance/useUpdateProductionPerformance';
+import useUpdateProductionPerformances from '@/apis/query-hooks/production-performance/useUpdateProductionPerformance';
 import { FormField, FormItem, FormLabel, FormControl } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import PPTable from '@/pages/production-management/production-performance/PPTable.vue';
 
 const route = useRoute();
-const { mutate: updateProductionPerformance } = useUpdateProductionPerformance(
+const { data: productionPerformanceDetail } = useGetProductionPerformance(route.params.id);
+const { mutate: updateProductionPerformance } = useUpdateProductionPerformances(
   route.params.productionPerformanceId,
 );
 
-const {
-  data: detail,
-  isLoading, // 로딩 상태 추가
-  isError, // 에러 상태 추가 // 에러 객체 추가
-} = useGetProductionPerformance(route.params.id);
+const form = useForm();
+const PPDetail = ref({});
+
+const onSubmit = form.handleSubmit(values => {
+  const params = {
+    remark: values.remark,
+  };
+
+  // @ts-ignore
+  updateProductionPerformance(params);
+});
+
+watch(
+  productionPerformanceDetail,
+  val => {
+    if (!val) return;
+    form.setValues({
+      factoryName: `${val.factoryName} (${val.factoryCode})`,
+      lineName: `${val.lineName} (${val.lineCode})`,
+      itemName: val.itemName,
+      dueDate: val.dueDate,
+      salesManagerName: `${val.salesManagerName} (${val.salesManagerNo})`,
+      productionManagerName: `${val.productionManagerName} (${val.productionManagerNo})`,
+      startTime: val.startTime,
+      endTime: val.endTime,
+      lotNo: val.lotNo,
+      remark: val.remark,
+    });
+
+    PPDetail.value = {
+      itemCode: val.itemCode,
+      itemName: val.itemName,
+      itemSpecification: val.itemSpecification,
+      itemUnit: val.itemUnit,
+      totalQty: val.totalQty,
+      performanceQty: val.performanceQty,
+      defectiveQty: val.defectiveQty,
+      defectiveRate: val.defectRate,
+    };
+  },
+  { immediate: true },
+);
 </script>
 
 <style scoped></style>
