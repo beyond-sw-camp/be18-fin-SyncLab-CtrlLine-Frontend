2e1b1437f96430d15db73209e26d8287e59f6cf2
kimeodml <kimeodml@gmail.com>
2025-12-11 23:53:55 +0900

    [Feat] 스크롤 디자인 변경

--- src/pages/production-management/production-plan/CreateScheduleData.vue
+++ src/pages/production-management/production-plan/CreateScheduleData.vue
@@ -297,6 +297,24 @@ function onPopupOpen(args) {
   overflow: visible !important;
 }
 
+/* 스크롤바 지정 */
+.e-schedule .e-content-wrap::-webkit-scrollbar,
+.e-schedule::-webkit-scrollbar {
+  width: 2px; /* 원하는 너비로 설정하여 얇게 만듭니다. */
+  height: 2px;
+}
+
+.e-schedule .e-content-wrap::-webkit-scrollbar-thumb,
+.e-schedule::-webkit-scrollbar-thumb {
+  background: gray;
+  border-radius: 3px;
+}
+
+.e-schedule .e-content-wrap::-webkit-scrollbar-track,
+.e-schedule::-webkit-scrollbar-track {
+  background: transparent;
+}
+
 /* 막대바 높이 지정 */
 .e-schedule .e-timeline-view .e-content-wrap tr,
 .e-schedule .e-timeline-view .e-resource-column-wrap tr {
@@ -324,7 +342,12 @@ function onPopupOpen(args) {
   width: 2px !important;
 }
 
-.time-scale.e-schedule .e-header-row .e-time-slots {
-  display: none !important;
+.time-scale.e-schedule .e-header-row .e-time-slots span {
+  display: block;
+  white-space: normal;
+  word-wrap: break-word;
+  font-size: 8px;
+  width: 20px;
+  height: 30px;
 }
 </style>
